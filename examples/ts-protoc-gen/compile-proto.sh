1263d7375a04b71820ff52087b04e27ed2d07d99
Richard Willis <willis.rh@gmail.com>
2020-12-17 19:34:24 +0000

    Update compile-proto.sh

--- examples/ts-protoc-gen/compile-proto.sh
+++ examples/ts-protoc-gen/compile-proto.sh
@@ -10,7 +10,7 @@ PROTOC_GEN_GRPC_PATH="$(npm bin)/grpc_tools_node_protoc_plugin"
 $PROTOC \
     -I="./" \
     --plugin=protoc-gen-ts=$PROTOC_GEN_TS_PATH \
-    --plugin=protoc-gen-grpc=${PROTOC_GEN_GRPC_PATH} \
+    --plugin=protoc-gen-grpc=$PROTOC_GEN_GRPC_PATH \
     --js_out=import_style=commonjs:$OUT_DIR \
     --grpc_out=grpc_js:$OUT_DIR \
     --ts_out=service=grpc-node,mode=grpc-js:$TS_OUT_DIR \
